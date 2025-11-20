c051ba8d7ab2b13f4cc545ab2b84dfb08c960635
bangchaewon <118799810+chaewon02@users.noreply.github.com>
2025-11-20 16:55:35 +0900

    [Gold V] Title: 탑, Time: 924 ms, Memory: 90780 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,28 +5,36 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+// 탑 (골드)
+// 시간초과 35분 / 1시간
+// 스택이 인덱스와, 높이 기억하는게 포인트
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
+        int[] arr = new int[n]; // 탑 높이 배열
         Stack<int[]> stack = new Stack<>();
-        for(int i = 1; i <= n; i++) {
-            int top = Integer.parseInt(st.nextToken());
-            while(!stack.isEmpty()) {
-                if(stack.peek()[1] >= top) {
-                    System.out.print(stack.peek()[0] + " ");
-                    break;
-                }
-                stack.pop();
-            }
-            if(stack.isEmpty()) {
-                System.out.print("0 ");
-            }
-            stack.push(new int[] {i, top});
-        }
-    }
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int val = Integer.parseInt(st.nextToken());
+
+            while(!stack.isEmpty()){
+                if(val <= stack.peek()[1]){
+                    sb.append(stack.peek()[0]).append(" ");
+                    break;
+                }else{
+                    stack.pop();
+                }
+            }
+            if(stack.isEmpty()){
+                sb.append("0 ");
+            }
+            stack.push(new int[]{i+1,val});
+
+        }
+        System.out.println(sb);
+
+    }
 }
