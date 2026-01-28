7526984ce45601ed2421287a16e49c2b741fb999
bangchaewon <118799810+chaewon02@users.noreply.github.com>
2026-01-29 02:33:41 +0900

    [Silver I] Title: 미로 탐색, Time: 100 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,59 +1,58 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main {
+// 07 33
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tk = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(tk.nextToken());
-        int M = Integer.parseInt(tk.nextToken());
-        int map[][] = new int[N][M];
-        boolean visited[][] = new boolean[N][M];
-        for(int i=0;i<N;i++){
-            String s = br.readLine();
-            for(int j=0;j<M;j++){
-                map[i][j] = s.charAt(j) - '0';
-            }
-        }
-        // 풀이
-        int dx[] = {-1,1,0,0};
-        int dy[] = {0,0,-1,1};
-        int dist[][] = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dist[i][j] = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][m];
+        for(int i=0;i<n;i++){
+            String str = br.readLine();
+            for(int j=0;j<m;j++){
+                map[i][j] = Character.getNumericValue(str.charAt(j));
+            //str.charAt(j)-'0';
+                //Character.getNumericValue(str.charAt(j))
             }
         }
 
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(visited[i][j] || map[i][j]==0) continue;
-                visited[i][j] = true;
-                Queue<int[]> q = new LinkedList<>();
-                q.offer(new int[]{i,j});
-                dist[i][j]++;
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+        boolean[][] visited = new boolean[n][m];
+        int cnt = 1;
 
-                while (!q.isEmpty()){
-                    int[] temp = q.poll();
-                    int x = temp[0];
-                    int y = temp[1];
-                    for(int k=0;k<4;k++){
-                        int nx = x + dx[k];
-                        int ny = y + dy[k];
-                        if(nx<0||ny<0||nx>=N||ny>=M) continue;
-                        if(map[nx][ny]==0 || visited[nx][ny]) continue;
-                        visited[nx][ny] = true;
-                        q.offer(new int[]{nx,ny});
-                        dist[nx][ny] = dist[x][y]+1;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0,0,cnt});
+        visited[0][0] = true;
+
+        while(!queue.isEmpty()){
+            int[] poll = queue.poll();
+            int y = poll[0];
+            int x = poll[1];
+            int len = poll[2];
+            for(int k=0;k<4;k++){
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                if(nx<0||ny<0||nx>=m||ny>=n) continue;
+                if(map[ny][nx]==1 && !visited[ny][nx]){
+                    if(ny==n-1 && nx==m-1){
+                        System.out.println(len+1);
+                        return;
                     }
+                    queue.add(new int[]{ny,nx,len+1});
+                    visited[ny][nx] = true;
                 }
             }
         }
-        System.out.println(dist[N-1][M-1]);
-
+        //System.out.println(cnt);
     }
 }
