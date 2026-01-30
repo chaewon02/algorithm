09296d8a497bb83edd7f0b3af2f53a4e5d368c86
bangchaewon <118799810+chaewon02@users.noreply.github.com>
2026-01-30 20:25:58 +0900

    [Silver IV] Title: 동전 0, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,32 +1,27 @@
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-//05 10 15 20
+//14
 public class Main {
-    static ArrayList<Integer> count = new ArrayList<>();
-    static int min = 1000000001;
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-//        boolean[] check = new boolean[n];
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        ArrayList<Integer> coin = new ArrayList<>();
+        int res = 0;
+        for(int i =0;i<n;i++){
+            coin.add(sc.nextInt());
         }
-        greedy(n-1,0,k,arr);
 
-        System.out.println(min);
-
-    }
-    static void greedy(int i,int c, int k, int[] arr){
-        if(i < 0){
-            if(k ==0 && c < min){
-                min = c;
+        for(int i=n-1;i>=0;i--){
+            if(k!=0 && coin.get(i)<=k){
+                res += k / coin.get(i);
+                k = k % coin.get(i);
             }
-        }else{
-            greedy(i-1,c+(k/arr[i]),k%arr[i],arr);
-            greedy(i-1,c,k,arr);
         }
+
+        System.out.println(res);
     }
 }
